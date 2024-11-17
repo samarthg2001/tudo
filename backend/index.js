27844667345345eb11a627a4ba9c860549f0cd56
Samarth <155800208+samarthg2001@users.noreply.github.com>
2024-11-17 16:45:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ import cors from 'cors'
 const port=8000
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    origin:true
+))
 Dbconnection()
 app.listen(port||process.env.PORT
     ,()=>{
