46e74c0626072de7eee11d988b8ec98730aa370c
Samarth <155800208+samarthg2001@users.noreply.github.com>
2024-11-17 16:20:26 +0530

    Update SignUp.jsx

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -25,7 +25,7 @@ const submit = async (e) => {
   try {
     console.log(input);
     console.log(e);
-    const res = await axios.post("http://localhost:8000/api/register", input);
+    const res = await axios.post("https://tudo-gules.vercel.app/?vercelToolbarCode=XZb3AFJLvOVE8lY/api/register", input);
       setInput({
           email: "",
           name: "",
