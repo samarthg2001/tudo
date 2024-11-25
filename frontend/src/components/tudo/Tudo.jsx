de2c65a80f08b5750378658c53eeff3076b9e85c
Samarth <155800208+samarthg2001@users.noreply.github.com>
2024-11-25 18:11:43 +0530

    Update Tudo.jsx
    css import missmatch

--- frontend/src/components/tudo/Tudo.jsx
+++ frontend/src/components/tudo/Tudo.jsx
@@ -5,8 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import TudoCard from './TudoCard';
 import { GrUpdate } from 'react-icons/gr';
 import { FaRegWindowClose } from 'react-icons/fa';
-import './tudoCard.css';
-import './UPdate.css';
+import './TudoCard.css';
+import './Update.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 
